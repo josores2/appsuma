3cf0130417a6819bdcb5d453136112e942392024
Jose Sorli <jose.sorli.esteve@hotmail.com>
2024-01-23 17:59:34 +0100

    En esta nueva versión, hemos añadido un fascinante nuevo botón que, de forma totalmente alucinante, nos va a restar dos números

---
+++ app/src/main/java/com/josesorli/sumar2numeros/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.josesorli.sumar2numeros
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val num1:EditText = findViewById(R.id.editTextNumber)
+        val num2:EditText = findViewById(R.id.editTextNumber2)
+        val boton1: Button = findViewById(R.id.button)
+        val boton2: Button = findViewById(R.id.button2)
+        val res1:TextView = findViewById(R.id.textView)
+
+        boton1.setOnClickListener {
+            //val resultado = (num1.text.toString().toInt()) + (num2.text.toString().toInt())
+            //res1.text = resultado.toString()
+
+            res1.text = ((num1.text.toString().toInt()) + (num2.text.toString().toInt())).toString()
+
+        }
+        boton2.setOnClickListener {
+            //val resultado = (num1.text.toString().toInt()) + (num2.text.toString().toInt())
+            //res1.text = resultado.toString()
+
+            res1.text = ((num1.text.toString().toInt()) - (num2.text.toString().toInt())).toString()
+
+        }
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Sumar 2 numeros"
+include(":app")
